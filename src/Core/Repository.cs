ddcdf85003d9f0c29eb5cc3f26e2158479cb8731
JonaMrk <jonamerkohasani@gmail.com>
2026-02-08 20:20:55 +0200

    Implement commit creation and persistent storage
    Added CreateCommit method to generate and save commits.

--- src/Core/Repository.cs
+++ src/Core/Repository.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using Storage;
 
 namespace Core
 {
@@ -18,6 +20,31 @@ namespace Core
       Directory.CreateDirectory(Path.Combine(RepoPath, ".gitlite", "objects"));
       Directory.CreateDirectory(Path.Combine(RepoPath, ".gitlite", "refs"));
     }
+
+    public Commit CreateCommit(string message, string parentcommitId = "")
+    {
+      string content = message + DateTime.Now.ToString();
+      string commmitId = Hasher.ComputeHash(content);
+
+      Commit commit = new Commit(commitId, parentCommitId, message);
+      SaveCommit(commit);
+
+      return commit;
+    }
+
+    private void Savecommit(Commmit commit)
+    {
+      string objectsPath = Path.Combine(RepoPath, ".gitlite", "objects");
+      string commitFilePath = Path.Combine(objectsPath, commit.CommitId + ".txt");
+
+      string fileContent = 
+        "Commit ID: " + commit.CommitId + Environment.NewLine +
+        "Parent: " + commit.ParentCommitId + Environment.NewLine +
+        "Message: " + commit.Message + Environment.NewLine +
+        "TimeStamp: " + commit.Timestamp;
+
+      File.WriteAllText(commitFilePath, fileContent);
+      }
   }
 }
 
